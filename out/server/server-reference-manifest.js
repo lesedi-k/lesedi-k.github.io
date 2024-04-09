5850ee7ee3b326f24cecbd38bd9ce3010b8f1cdd
lesedi-k <97709988+lesedi-k@users.noreply.github.com>
2024-04-09 01:58:36 -0400

    changed file structure

--- out/server/server-reference-manifest.js
+++ out/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"coF0yg3NybZhRzoYPxBQnPf3h3iD4YEEfun1kwAMcYE=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"NpcbqRJsud3MGw/yfoBBECTriBcFxzvoYcy+bd8OOYk=\"\n}"
